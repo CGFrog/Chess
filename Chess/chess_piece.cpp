1022ddf0ba5e32ead90f000f09af91f6dd717763
Ian Cooper <iancoop25@gmail.com>
2024-04-18 12:09:45 -0500

    Getters and Setters for Chess Pieces

--- Chess/chess_piece.cpp
+++ Chess/chess_piece.cpp
@@ -1,7 +1,37 @@
 #include "chess_piece.h"
 
-chess_piece::chess_piece(bool white, int posx, int posy, char symbol) {
-	this->white = white;
+chess_piece::chess_piece(bool is_white, int posx, int posy, char symbol) {
+	this->is_white = is_white;
 	this->symbol = symbol;
+	this->posx = posx;
+	this->posy = posy;
+}
+//GETTERS
+bool chess_piece::get_in_danger() const {
+	return in_danger;
+}
+bool chess_piece::get_is_white() const {
+	return is_white;
 
 }
+int chess_piece::get_posx() const {
+	return posx;
+}
+int chess_piece::get_posy() const {
+	return posy;
+}
+char chess_piece::get_symbol() const {
+	return symbol;
+}
+
+//SETTERS
+void chess_piece::set_in_danger(bool in_danger) {
+	this -> in_danger = in_danger;
+}
+void chess_piece::set_posx(int posx) {
+	this->posx = posx;
+}
+void chess_piece::set_posyx(int posy) {
+	this->posy = posy;
+}
+
--- Chess/chess_piece.h
+++ Chess/chess_piece.h
@@ -1,15 +1,32 @@
 #pragma once
+#include <string>
 
 class chess_piece {
 
-public:
-
 	bool in_danger = false;
-	bool white;
-	int pos[2] = {};
+	bool is_white;
+	int posx;
+	int posy;
 	char symbol;
 
+
+public:
+
+	bool get_in_danger() const;
+	bool get_is_white() const;
+	int get_posx() const;
+	int get_posy() const;
+	char get_symbol() const;
+
+	void set_in_danger(bool in_danger);
+	void set_posx(int posx);
+	void set_posyx(int posy);
+
+
+
 	chess_piece(bool white, int posx, int posy, char symbol);
+
+	void move_piece(int posx, int posy, std::string next_pos);
 
 };
 
